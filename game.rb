36aa3a9dd71bff45a9103ea37935f89d652c95db
Tim Uruski <github@login.timuruski.name>
2013-08-28 07:58:45 -0600

    Figure out how to draw boxes!

--- game.rb
+++ game.rb
@@ -1,9 +1,11 @@
 #! /usr/bin/env ruby
+# encoding: utf-8
 
 # The Game of Life
 # ================
 
 
+require 'io/console'
 require 'optparse'
 require 'ostruct'
 
@@ -19,8 +21,11 @@ module Game
       @running = false
     end
 
+    attr_reader :config
+
     def start
       @running = true
+      IO.console.echo = false
       handle_signals
 
       clear
@@ -31,23 +36,68 @@ module Game
         sleep 0.3
       end
 
+      IO.console.echo = false
       sleep 0.2
       clear
     end
 
+    HBORDER = "━"
+    VBORDER = "┃"
+    TLCORNER = "┏"
+    TRCORNER = "┓"
+    BLCORNER = "┗"
+    BRCORNER = "┛"
+
     def draw
-      @dots ||= ['   ', '.  ', '.. ', '...'].cycle
-      line "Draw#{@dots.next}\n"
-      line "Draw#{@dots.next}\n"
-      line "Draw#{@dots.next}\n"
+      win_height, win_width = IO.console.winsize
+      box_height, box_width = [20, 72]
+
+      box = ''.tap do |s|
+        box_height.times do |y|
+          box_width.times do |x|
+            if y == 0 && x == 0
+              c = TLCORNER
+            elsif y == 0 && x == box_width - 1
+              c = TRCORNER
+            elsif y == box_height - 1 && x == 0
+              c = BLCORNER
+            elsif y == box_height - 1 && x == box_width - 1
+              c = BRCORNER
+            elsif y == 0 || y == box_height - 1
+              c = HBORDER
+            elsif x == 0 || x == box_width - 1
+              c = VBORDER
+            else
+              c = " "
+            end
+
+            s << c
+          end
+          s << "\n"
+        end
+      end
+
+      IO.console.puts box
+    end
+
+    def width
+      config.size
+    end
+
+    def height
+      config.size
     end
 
     def line(str = '')
       puts "\e[0K" + str
     end
 
+    def position_cursor(y,x)
+      print "\e[#{y};#{x}"
+    end
+
     def reset_cursor
-      print "\e[H"
+      print "\e[f"
     end
 
     def clear
@@ -67,7 +117,7 @@ module Game
   module Config
     def self.parse(args)
       config = OpenStruct.new
-      config.size = 16
+      config.size = 2
     end
   end
 
